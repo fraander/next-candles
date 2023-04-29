ed80a4724ba53e8f9d56f3322b65c43392d618a9
fraander <57777918+fraander@users.noreply.github.com>
2023-04-29 13:59:26 -0400

    starting to rework feed view

--- Next Candles/Next Candles/Views/ContentView.swift
+++ Next Candles/Next Candles/Views/ContentView.swift
@@ -50,5 +50,6 @@ struct ContentView: View {
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
+            .environmentObject(ContactsVM())
     }
 }
