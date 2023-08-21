583b8076063ac5d989675b6d37e27f45a9e4f2c2
fraander <57777918+fraander@users.noreply.github.com>
2023-08-20 21:02:40 -0500

    Update SettingsVM.swift

--- Next Candles/Next Candles/Model/SettingsVM.swift
+++ Next Candles/Next Candles/Model/SettingsVM.swift
@@ -11,5 +11,8 @@ import Foundation
  Store all Settings such as to show only favorites.
  */
 class SettingsVM: ObservableObject {
-    @Published var nextUpDays: Double = 20
+    @Published var nextUpDays: Double = 20;
+//     @Published var hidden: [UUID] = [];
+    
+    // TODO: Allow for hiding of contacts using UserDefaults (store list of hidden contacts by ID)
 }
