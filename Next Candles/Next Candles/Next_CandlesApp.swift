9c0cb1a15ceddd417c56b3675816e1d40335ebb8
fraander <57777918+fraander@users.noreply.github.com>
2023-11-22 09:29:56 -0500

    log notes from first run

--- Next Candles/Next Candles/Next_CandlesApp.swift
+++ Next Candles/Next Candles/Next_CandlesApp.swift
@@ -7,20 +7,27 @@
 
 import SwiftUI
 
+/*
+ Bugs
+ - When removing the last contact from a month, the month sticks around
+ - Loading for the first time brings the whole app to a halt -- setup some "import" screen to get around this
+ - Names that are long mixed with dates that are long has a weird rendering error. Is there a way to shorten dates / truncate
+ 
+ Features
+ - Birthday reminders x days out
+ - Highlight upcoming in x days
+ - Tap to detail to show contact card
+ - Better import management
+ - Show/hide hidden
+ */
+
 @main
 struct Next_CandlesApp: App {
-    
-    //    @StateObject var settings = SettingsVM()
-    //    @StateObject var contacts = ContactsVM()
     
     var body: some Scene {
         WindowGroup {
             ContactListView()
                 .modelContainer(for: Contact.self)
-            
-            //            ContentView()
-            //                .environmentObject(settings)
-            //                .environmentObject(contacts)
             
         }
     }
